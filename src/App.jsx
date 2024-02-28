c0dd14d94a6b5eccabe9ff847d107a681449ab6a
Deep Furiya <79759607+deepfuriya@users.noreply.github.com>
2024-02-27 20:41:06 -0500

    experience section done

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,8 @@ import AboutMe from './components/AboutMe';
 import Skills from './components/Skills';
 import React from 'react';
 import { SKILLS } from './constants';
+import { EXPERIENCE } from './constants';
+import Experience from './components/Experience';
 
 
 function App() {
@@ -45,11 +47,36 @@ function App() {
                 {Object.values(SKILLS).map((key, index) => (
                   <Skills key={index} heading={key.title} skillsArray={key.content} icon={key.icon}/>
                 ))}
-                
-
           </div>
-       
+
       </section>
+
+      {/* Experience Section */}
+      <section class="bg-light py-5" id="experience">
+        <div class="container px-5">
+            <div class="row gx-5 justify-content-center">
+                <div class="">
+                    <div class="text-center mt-5">
+                        <h2 class="display-5 fw-bolder"><span class="text-gradient d-inline">Experience</span></h2>
+                        
+                        <div class="card shadow border-0 rounded-4 mb-5 mt-3">
+                            <div class="card-body p-5">
+                                <div class="row align-items-center gx-5">
+                                    
+                                    {Object.values(EXPERIENCE).map((key,index) => (
+                                        <Experience key={index} value={key}/>
+                                    ))}
+
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
 
     </div>
--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,7 @@
+import cci from './images/logo/unc-cci.png';
+import usc from './images/logo/usc.png';
+import top10 from './images/logo/top-10.png';
+
 export const DATA = {
     LINKEDIN: "https://www.linkedin.com/in/deepfuriya/",
     GITHUB: "https://github.com/deepfuriya",
@@ -21,5 +25,32 @@ export const SKILLS = {
         title: "Application Development",
         icon: "bi-braces",
         content: ["Android Development ( Java / Kotlin )", "Web Development ( Node.js )", "API Development ( PHP )"]
+    }
+};
+
+export const EXPERIENCE = {
+    ta: {
+        title: "Graduate Teaching Assistant",
+        duration: "Jan 2023 - Present",
+        location: "Charlotte, NC",
+        employer: "College of Computing & Informatics",
+        link: "https://cci.charlotte.edu/",
+        logo: cci
+    },
+    ra: {
+        title: "Research Assistant",
+        duration: "June 2023 - August 2023",
+        location: "Columbia, SC",
+        employer: "University of South Carolina",
+        link: "https://sc.edu/",
+        logo: usc
+    },
+    fullStackDev: {
+        title: "Full Stack Developer",
+        duration: "August 2021 - July 2022",
+        location: "Mumbai, India",
+        employer: "Top 10 Mobiles",
+        link: "https://top10mobiles.com/",
+        logo: top10
     }
 };
